d1ccdee29027fb9f3377ae476a3f405de7aac786
tellingyt8303-art <tellingyt8303@gmail.com>
2026-08-06 22:33:02 +0300

    Add files via upload

---
+++ src/middlewares/roleMiddleware.js
@@ -0,0 +1,13 @@
+const authorizeRoles = (...roles) => {
+  return (req, res, next) => {
+    if (!req.user || !roles.includes(req.user.role)) {
+      return res.status(403).json({
+        success: false,
+        message: `Role '${req.user ? req.user.role : 'unknown'}' is not allowed to access this resource`,
+      });
+    }
+    next();
+  };
+};
+
+module.exports = authorizeRoles;
